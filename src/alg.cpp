9bd84119da87f3c3f7d88962ce78c23493ffa6ca
rostovtsev-nikita <99068528+rostovtsev-nikita@users.noreply.github.com>
2022-03-11 17:14:06 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,27 @@
 // Copyright 2021 NNTU-CS
 
-int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+int cbinsearch(int* arr, int size, int value) {
+int O = 0, e = size - 1;
+while (O < e) {
+int mid = O + (e - O) / 2;
+if (arr[mid] == value) {
+int a = 0, b = 0;
+for (a = mid - 1; a >= O; a--) {
+if (arr[a] != value)
+break;
+}
+for (b = mid + 1; b <= e; b++) {
+if (arr[b] != value)
+break;
+}
+return (b - 1) - a;
+}
+else if (arr[mid] > value) {
+e = mid;
+}
+else {
+O = mid + 1;
+}
+}
+return 0;
 }
